5a4fe30ce15504e3767bfa264d858f49a948ff03
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-04-05 14:01:39 -0300

    Exercício resolvido.

--- Extra07.js
+++ Extra07.js
@@ -2,3 +2,10 @@
 // Na sequência, defina uma variável para o valor da taxa de serviço que é de 10%. 
 // Por fim defina uma variável que será responsável pelo cálculo do valor total da conta e exiba-o no console 
 // com a seguinte formatação: R$99.99 (valor com duas casas decimais).
+
+let refeicao = 42.54
+let taxa = 0.1
+
+let total = ((taxa * refeicao) + refeicao).toFixed(2)
+
+console.log(`O valor da refeição foi de R${refeicao} + 10%. Total: R$${total}.`)
--- Extra08.js
+++ Extra08.js
@@ -1,3 +1,15 @@
-// Qual o valor do troco? - Crie um programa que calcule o troco de uma compra no valor de R$100,98 e 
-// que foi paga com R$150,00. 
+// Qual o valor do troco? - Crie um programa que calcule o troco de uma compra no valor de R$100,98 e
+// que foi paga com R$150,00.
 // O valor do troco deve ser exibido no console.
+
+console.log(`Cálculo de troco.`);
+let compra = 100.98;
+let valorPago = 150;
+
+console.log(`Valor da compra R$${compra}. \nValor pago R$${valorPago}.`);
+
+let resultado;
+
+resultado = (valorPago - compra).toFixed(2);
+
+console.log(`Seu troco é de R$${resultado}.`);
