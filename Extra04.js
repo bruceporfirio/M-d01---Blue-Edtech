c79732b30b4b44163671d6c87536f6474e2de0b1
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-04-05 14:48:04 -0300

    Exercício resolvido.

--- Extra04.js
+++ Extra04.js
@@ -1,6 +1,6 @@
-// Tabela de notas - Você foi contratado ou contratada por uma escola pra fazer o sistema de boletim dos alunos. 
+// Tabela de notas - Você foi contratado ou contratada por uma escola pra fazer o sistema de boletim dos alunos.
 // Como primeiro passo, escreva um programa que produza a seguinte saída:
-// ALUNO (A) 
+// ALUNO (A)
 // NOTA
 // ALINE
 //  9.0
@@ -10,3 +10,21 @@
 //   4.5
 // SHIRLEY
 //   7.0
+
+const prompt = require('prompt-sync')();
+
+let alunoA = prompt(`Digite o nome do aluno:`);
+let notaA = prompt(`Digite a nota do aluno:`);
+let alunoB = prompt(`Digite o nome do aluno:`);
+let notaB = prompt(`Digite a nota do aluno:`);
+let alunoC = prompt(`Digite o nome do aluno:`);
+let notaC = prompt(`Digite a nota do aluno:`);
+let alunoD = prompt(`Digite o nome do aluno:`);
+let notaD = prompt(`Digite a nota do aluno:`);
+
+console.log(`
+ALUNO(a)                               ||                                   NOTA
+${alunoA}                              ||                                   ${notaA}
+${alunoB}                              ||                                   ${notaB}
+${alunoC}                              ||                                   ${notaC}
+${alunoD}                              ||                                   ${notaD}           `);
