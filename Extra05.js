025dc17a1b5f22f2cf7a75e7307a17a63b49bd3d
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-04-05 14:36:04 -0300

    Exercício resolvido.

--- Extra05.js
+++ Extra05.js
@@ -1,2 +1,24 @@
-// Faça um programa de cadastro de clientes que mostre um menu de opções 
+// Faça um programa de cadastro de clientes que mostre um menu de opções
 // e permita com que a pessoa escolha umas das opções, exibindo qual foi a opção escolhida.
+const prompt = require(`prompt-sync`)();
+console.log('Escolha sua refeição:');
+
+let opc1 = `Fritas`;
+let opc2 = `Shake`;
+let opc3 = `Hambúrguer`;
+
+console.log(`1 - Fritas \n2 - Shake \n3 - Hambúrguer`);
+
+let escolha = (+prompt(
+    `Digite o número correspondente a seu pedido: `,
+)).toFixed(0);
+
+if (escolha == 1) {
+    console.log(`Você escolheu ${opc1}.`);
+} else if (escolha == 2) {
+    console.log(`Você escolheu ${opc2}.`);
+} else if (escolha == 3) {
+    console.log(`Você escolheu ${opc3}.`);
+} else {
+    console.log(`Opção digitada inválida!`);
+}
