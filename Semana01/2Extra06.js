e6a565e5e497cebe9b1ebd8b9e041b7bf41f01b6
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-04-06 18:27:54 -0300

    .

--- Semana01/2Extra06.js
+++ Semana01/2Extra06.js
@@ -4,3 +4,11 @@
 // o monstro (o jogador irá derrotar o monstro em 8 turnos).
 
 const prompt = require("prompt-sync")();
+
+const vidaMonstro = +prompt(`Digite a quantidade de vida do monstro (entre 10 e 50): `);
+
+const danoJogador = +prompt(`Digite o valo rde ataque do jogador (entre 5 e 10): `);
+
+const turnos = Math.ceil(vidaMonstro / danoJogador); //Função Math.ceil vai arredondar o valor para cima. 
+
+console.log(`O jogador irá derrotar o monstro em  ${turnos} turnos.`)
