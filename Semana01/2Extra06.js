8b2ca60902ee8c3c0422d6cf5e95c88bba5633a2
Bruce Porfirio <99917030+bruceporfirio@users.noreply.github.com>
2022-04-06 18:18:13 -0300

    Mudança de estruturação de local de arquivos.

--- Semana01/2Extra06.js
+++ Semana01/2Extra06.js
@@ -2,3 +2,5 @@
 // quantidade de vida de um monstro (entre 10 e 50) e valor do ataque do jogador por turno (entre 5 e 10). 
 // Baseado nos valores digitados, exiba a quantidade de turnos que o jogador irá demorar para conseguir derrotar 
 // o monstro (o jogador irá derrotar o monstro em 8 turnos).
+
+const prompt = require("prompt-sync")();
